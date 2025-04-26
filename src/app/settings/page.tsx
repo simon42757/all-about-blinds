32a5cb3d3a9968da79ecc05fd4e57605c36c4daa
simon42757 <simon.tucker@hotmail.co.uk>
2025-04-26 11:56:55 +0100

    Navigate to main page after saving settings

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -69,6 +69,8 @@ export default function Settings() {
   
   const handleSaveConfirmClose = () => {
     setShowSaveConfirmation(false);
+    // Navigate back to the main page
+    router.push('/');
   };
 
   return (
